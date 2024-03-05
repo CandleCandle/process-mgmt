6c4ee55e7c15b92c0ae6bd06c2dfd318eb7f627f
Chris West (Faux) <git@goeswhere.com>
2024-03-05 14:17:17 +0000

    feat: actually compile ts code

--- src/process.ts
+++ src/process.ts
@@ -109,7 +109,7 @@ class ProcessChain {
     /**
      * @param arguments process, process, ...
      */
-    enable() {
+    enable(..._args) {
         this._enable(arguments);
         this._rebuild();
         return this;
