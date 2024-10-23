43a13f02319d3440d258e60ac2cc1ad4795b3b1f
candle <candle@candle.me.uk>
2024-10-23 18:37:06 +0100

    Add quality-less data for Factorio-SA

---
+++ src/factorio-2.0.7-sa-1.0.0/data.js
@@ -0,0 +1,11 @@
+import { default as create_data } from '../factorio-recipe-lister/data_base.js';
+
+const _import_file = function (name) {
+    return import('./recipe-lister/' + name, { assert: { type: 'json' } })
+        .catch((e) => {
+            console.log('failed to read recipe.json:', e);
+        })
+        .then((m) => m.default);
+};
+
+export default await create_data('factorio-2.0.7-sa', '1.0.0', _import_file);
